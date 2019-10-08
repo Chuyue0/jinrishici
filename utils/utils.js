899f32b29739c336812c875ad7521b6b3c37fde8
Roséa'’ <1084224413@qq.com>
2019-10-08 15:56:41 +0800

    Add files via upload

---
+++ utils/utils.js
@@ -0,0 +1,17 @@
+function getSystemInfo() {
+	//获取系统消息
+	return new Promise((resolve, reject) => {
+		uni.getSystemInfo({
+			success(res) {
+				resolve(res)
+			},
+			fail(err) {
+				reject(err)
+			}
+		})
+	})
+}
+
+module.exports = {
+	getSystemInfo,
+}
