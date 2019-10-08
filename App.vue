f8340776ba5117cec69267dec51bf37b5923893e
Roséa'’ <1084224413@qq.com>
2019-10-08 15:56:22 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		globalData:{
+			//uni-app引入globalData概念，vue无
+			title: '今日诗词'
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
